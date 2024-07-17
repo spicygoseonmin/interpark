e780e137f5e20495002da9e7043f6841eb33458a
seonmin <seonmin1414@gmail.com>
2024-07-17 13:17:47 +0900

    2024. 07. 17 (수) 티켓 섹션 추가 및 반응형 수정, 슬라이드 색션 루프 추가

--- js/index.js
+++ js/index.js
@@ -2,6 +2,29 @@ window.onload = function () {
     const mypage = document.querySelector(".mypage-bt");
     const mypagelist =document.querySelector(".mypage-list");
     mypage.onclick = function(){
-        mypagelist.classList.toggle("openMypage")
+        mypagelist.classList.toggle("openMypage");
     }
-}
+
+    // 프로모션 스와이프 기능
+    const swpromotion = new Swiper(".sw-promotion", {
+        slidesPerView: 1,
+        spaceBetween: 24,
+        breakpoints: {
+            480 : {
+                slidesPerView: 2,
+            },
+            760 : {
+                slidesPerView: 2,
+            }
+        },
+        navigation: {
+            nextEl: ".promotiom .sw-next",
+            prevEl: ".promotiom .sw-prev",
+        },
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
+    });
+};
+
---
+++ js/main.js
@@ -0,0 +1,192 @@
+window.onload = function () {
+  // 상단의 마이페이지리스트 기능
+  const mypageBt = document.querySelector(".mypage-bt");
+  const mypageList = document.querySelector(".mypage-list");
+  mypageBt.addEventListener("click", function () {
+    mypageList.classList.toggle("showMypage");
+  });
+  // swiper ================
+  // 프로모션 스와퍼기능
+  const swPromotion = new Swiper(".sw-promotion", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+      },
+      760: {
+        slidesPerView: 2,
+      },
+    },
+    navigation: {
+      nextEl: ".promotion .sw-next",
+      prevEl: ".promotion .sw-prev",
+    },
+    pagination: {
+      el: ".sw-promotion-pg ",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+
+  // 쇼핑의 스와이퍼 기능적용
+  const swShopping = new Swiper(".sw-shopping", {
+    slidesPerView: 1.2,
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
+    spaceBetween: 10,
+    breakpoints: {
+      400: {
+        spaceBetween:20,
+        slidesPerView:2,
+        slidesPerGroup:2,
+        grid: {
+          rows: 1,
+        },
+      },
+      900: {
+        spaceBetween: 32,
+        slidesPerView: 4,
+        grid: {
+          rows: 1,
+        },
+        slidesPerGroup: 3,
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+    navigation: {
+      nextEl: ".shopping .sw-next",
+      prevEl: ".shopping .sw-prev",
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+  const swTour = new Swiper(".sw-tour", {
+    slidesPerView: 1.2,
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
+    spaceBetween: 26,
+    breakpoints: {
+      400: {
+        spaceBetween:20,
+        slidesPerView:2,
+        slidesPerGroup:2,
+        grid: {
+          rows: 1,
+        },
+      },
+      768: {
+        spaceBetween:20,
+        slidesPerView:2,
+        slidesPerGroup:2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+
+  // 티켓 슬라이드 기능
+  const swTicket = new Swiper(".sw-ticket", {
+    slidesPerView: 1.2,
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
+    spaceBetween: 26,
+    breakpoints: {
+      400: {
+        spaceBetween:20,
+        slidesPerView:2,
+        slidesPerGroup:2,
+        grid: {
+          rows: 1,
+        },
+      },
+      768: {
+        spaceBetween:20,
+        slidesPerView:2,
+        slidesPerGroup:2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+    navigation: {
+      nextEl: ".ticket .sw-next",
+      prevEl: ".ticket .sw-prev",
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+};
